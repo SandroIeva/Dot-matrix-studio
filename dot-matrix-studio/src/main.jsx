033e7efeab2cb10d3cc67f3a9f23235ab0379e88
SandroIeva <mail@sandroieva.com>
2026-02-13 13:59:13 +0100

    Delete dot-matrix-studio directory

--- dot-matrix-studio/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import DotLogoTool from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DotLogoTool />
-  </React.StrictMode>,
-)
